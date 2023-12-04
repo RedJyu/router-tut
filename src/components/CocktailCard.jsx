a56ca4a9fde570af027c3974bc0ba13925777293
RedJyu <piotr.stezycki@protonmai.com>
2023-12-04 14:22:32 +0100

    navigation

--- src/components/CocktailCard.jsx
+++ src/components/CocktailCard.jsx
@@ -1,4 +1,23 @@
-const CocktailCard = () => {
-  return <h2>CocktailCard</h2>;
+import Wrapper from '../assets/wrappers/CocktailCard';
+import { Link, useOutletContext } from 'react-router-dom';
+
+const CocktailCard = ({ img, name, info, id, glass }) => {
+  // const data = useOutletContext();
+  // console.log(data);
+  return (
+    <Wrapper>
+      <div className='img-container'>
+        <img src={img} alt={name} className='img' />
+      </div>
+      <div className='footer'>
+        <h4>{name}</h4>
+        <h5>{glass}</h5>
+        <p>{info}</p>
+        <Link to={`/cocktail/${id}`} className='btn'>
+          Details
+        </Link>
+      </div>
+    </Wrapper>
+  );
 };
 export default CocktailCard;
--- src/pages/HomeLayout.jsx
+++ src/pages/HomeLayout.jsx
@@ -1,12 +1,20 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, useNavigation } from 'react-router-dom';
 import Navbar from '../components/Navbar';
 
 const HomeLayout = () => {
+  const navigation = useNavigation();
+  const isPageLoading = navigation.state === 'loading';
+  // context can be used to transfer everything like state etc
+  const value = 'some value';
   return (
     <>
       <Navbar />
       <section className='page'>
-        <Outlet />
+        {isPageLoading ? (
+          <div className='isLoading' />
+        ) : (
+          <Outlet context={{ value }} />
+        )}
       </section>
     </>
   );
