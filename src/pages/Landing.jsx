eb1625c76cc77f502dffdab3a782c330098e1e4d
RedJyu <piotr.stezycki@protonmai.com>
2023-12-04 15:05:23 +0100

    up

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -7,7 +7,7 @@ const searchURL = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=';
 export const loader = async () => {
   const searchTerm = '';
   const response = await axios.get(`${searchURL}${searchTerm}`);
-  console.log(response);
+  // console.log(response);
   return { drinks: response.data.drinks, searchTerm };
 };
 
