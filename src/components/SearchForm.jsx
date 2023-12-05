a30ecee07fe1b8398c6ecceeee0f143cc618983f
RedJyu <piotr.stezycki@protonmai.com>
2023-12-05 14:06:47 +0100

    search

--- src/components/SearchForm.jsx
+++ src/components/SearchForm.jsx
@@ -1,4 +1,22 @@
+import Wrapper from '../assets/wrappers/SearchForm';
+import { Form, useNavigation } from 'react-router-dom';
 const SearchForm = () => {
-  return <h2>SearchForm</h2>;
+  const navigation = useNavigation();
+  const isSubmitting = navigation.state === 'submitting';
+  return (
+    <Wrapper>
+      <Form className='form'>
+        <input
+          type='search'
+          name='search'
+          className='form-input'
+          defaultValue='rum'
+        />
+        <button type='submit' className='btn' disabled={isSubmitting}>
+          {isSubmitting ? 'searching' : 'search'}
+        </button>
+      </Form>
+    </Wrapper>
+  );
 };
 export default SearchForm;
--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,11 +1,13 @@
 import { useLoaderData } from 'react-router-dom';
 import axios from 'axios';
 import CocktailList from '../components/CocktailList';
+import SearchForm from '../components/SearchForm';
 
 const searchURL = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=';
 // loader must return something!!
-export const loader = async () => {
-  const searchTerm = '';
+export const loader = async ({ request }) => {
+  const url = new URL(request.url);
+  const searchTerm = url.searchParams.get('search') || '';
   const response = await axios.get(`${searchURL}${searchTerm}`);
   // console.log(response);
   return { drinks: response.data.drinks, searchTerm };
@@ -16,6 +18,7 @@ const Landing = () => {
 
   return (
     <>
+      <SearchForm />
       <CocktailList drinks={drinks} />
     </>
   );
