b73cb538e9462835b9001f70cb89d12aee197922
RedJyu <piotr.stezycki@protonmai.com>
2023-12-05 13:10:21 +0100

    up

--- src/pages/Newsletter.jsx
+++ src/pages/Newsletter.jsx
@@ -1,7 +1,8 @@
 const Newsletter = () => {
   return (
-    <form className='form'>
+    <form className='form' action='post'>
       <h4 style={{ textAlign: 'center', marginBottom: '2rem' }}>newsletter</h4>
+      {/* name */}
       <div className='form-row'>
         <label htmlFor='name' className='form-label'>
           name
@@ -15,6 +16,41 @@ const Newsletter = () => {
           defaultValue='Bob'
         />
       </div>
+      {/* last name */}
+      <div className='form-row'>
+        <label htmlFor='lastName' className='form-label'>
+          Last Name
+        </label>
+        <input
+          type='text'
+          className='form-input'
+          name='lastName'
+          id='lastName'
+          // for testing
+          defaultValue='Bobowskis'
+        />
+      </div>
+      {/* email */}
+      <div className='form-row'>
+        <label htmlFor='email' className='form-label'>
+          email
+        </label>
+        <input
+          type='text'
+          className='form-input'
+          name='email'
+          id='email'
+          // for testing
+          defaultValue='test@test.com'
+        />
+      </div>
+      <button
+        type='submit'
+        className='btn btn-block'
+        style={{ marginTop: '0.5rem' }}
+      >
+        submit
+      </button>
     </form>
   );
 };
