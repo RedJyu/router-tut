94917640d1e8abf7b2c6b4cd70b7b14df2bc75ca
RedJyu <piotr.stezycki@protonmai.com>
2023-12-05 12:59:13 +0100

    newsletter start

--- src/pages/Newsletter.jsx
+++ src/pages/Newsletter.jsx
@@ -1,4 +1,21 @@
 const Newsletter = () => {
-  return <h1>Newsletter</h1>;
+  return (
+    <form className='form'>
+      <h4 style={{ textAlign: 'center', marginBottom: '2rem' }}>newsletter</h4>
+      <div className='form-row'>
+        <label htmlFor='name' className='form-label'>
+          name
+        </label>
+        <input
+          type='text'
+          className='form-input'
+          name='name'
+          id='name'
+          // for testing
+          defaultValue='Bob'
+        />
+      </div>
+    </form>
+  );
 };
 export default Newsletter;
